b461d18dcb93d25919a4c382f109fcc00dcd33f4
Andrés Losada <nedrocoli@gmail.com>
2019-12-02 00:48:33 -0500

    Plantilla para el parcial

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { IntlProvider } from "react-intl";
+
+import localeEnLang from './locale/en.json';
+import localeEsLang from './locale/es.json';
+
+let lenguaje = () => {
+    if (navigator.language.includes('es')) {
+        return localeEsLang;
+    }
+    else {
+        return localeEnLang;
+    }
+}
+
+ReactDOM.render(
+    <IntlProvider locale={navigator.language} key={navigator.language} data={lenguaje()}>
+        <App locale = {navigator.language}/>
+    </IntlProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
